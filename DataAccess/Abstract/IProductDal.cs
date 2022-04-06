b22cdc387bede2b3f18c17d666b818d264e93cec
Nusret Onur AYDUMAN <onurtheunique@users.noreply.github.com>
2022-04-06 05:59:18 +0300

    ders 7 sonu

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -10,11 +10,10 @@ namespace DataAccess.Abstract
     public interface IProductDal
     {
         List<Product> GetAll();
-        void Add(Product product)
-        { }
-        void Update(Product product)
-        { }
-        void Delete(Product product)
-        { }
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        List<Product> GetAllByCategory(int catId);
+       
     }
 }
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+            new Product{ProductId=1,CategoryId=1, ProductName="bardak",UnitPrice=15,UnitsInStock=15},
+            new Product{ProductId=2,CategoryId=1, ProductName="kamera",UnitPrice=500,UnitsInStock=10},
+            new Product{ProductId=3,CategoryId=2, ProductName="telefon",UnitPrice=1500,UnitsInStock=2},
+            new Product{ProductId=4,CategoryId=2, ProductName="klavye",UnitPrice=150,UnitsInStock=65},
+            new Product{ProductId=5,CategoryId=2, ProductName="fare",UnitPrice=850,UnitsInStock=1},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _products.Remove(_products.Where(w=>w.ProductId==product.ProductId).FirstOrDefault());//benim kodum
+            _products.Remove(_products.SingleOrDefault(p => p.ProductId == product.ProductId)); //Engin hoca
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void  Update(Product product)
+        {
+            Product toUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            toUpdate.CategoryId = product.CategoryId; ;
+            toUpdate.ProductName = product.ProductName;
+            toUpdate.CategoryId = product.CategoryId;
+            toUpdate.UnitPrice = product.UnitPrice;
+            toUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+    }
+}
