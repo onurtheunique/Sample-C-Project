ac0c5013bbd708d66959fcbbdbd668b8f69a7649
Nusret Onur AYDUMAN <onurtheunique@users.noreply.github.com>
2022-11-03 22:15:21 +0300

    Ders 11 Sonu

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFrameWork;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //  Bu bir attribute 
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService; //Loosely coupled --> Gevşek bağlılık
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            //IProductService productService = new ProductManager(new EfProductDal()) ; ---> burası bağımlılık yaratıyor
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("GetByID")]
+        public IActionResult GetByID(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add (Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
